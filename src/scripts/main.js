ff38e1f1711b347e78a0b38d301b5fd45bbf9c84
Bowers96 <bowersc96@gmail.com>
2017-04-23 18:50:48 -0400

    Update site content to match web layout

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,28 +1,28 @@
-// Document content is loaded, lets do some JS.
-document.addEventListener("DOMContentLoaded", function(event) {
-	
-	var module = {};
-
-	module.animateNav = function() {
-		var scrolTrigger = window.innerWidth > 700 ? 225 : 152;
-		var topOffset = '60px';
-
-		// this = 'scroll' event
-		var nav = document.querySelector('.nav');
-
-		if (this.scrollY > scrolTrigger ) {
-			nav.classList.add('nav--scrolled');
-			document.querySelector('body').style.paddingTop = topOffset;
-		} else {
-			nav.classList.remove('nav--scrolled');
-			document.querySelector('body').style.paddingTop = 0;
-		}
-	};
-
-	module.init = function (argument) {
-		window.addEventListener('scroll', module.animateNav);
-	};
-
-	module.init();
-
-});
+// // Document content is loaded, lets do some JS.
+// document.addEventListener("DOMContentLoaded", function(event) {
+//
+// 	var module = {};
+//
+// 	module.animateNav = function() {
+// 		var scrolTrigger = window.innerWidth > 700 ? 225 : 152;
+// 		var topOffset = '60px';
+//
+// 		// this = 'scroll' event
+// 		var nav = document.querySelector('.nav');
+//
+// 		if (this.scrollY > scrolTrigger ) {
+// 			nav.classList.add('nav--scrolled');
+// 			document.querySelector('body').style.paddingTop = topOffset;
+// 		} else {
+// 			nav.classList.remove('nav--scrolled');
+// 			document.querySelector('body').style.paddingTop = 0;
+// 		}
+// 	};
+//
+// 	module.init = function (argument) {
+// 		window.addEventListener('scroll', module.animateNav);
+// 	};
+//
+// 	module.init();
+//
+// });
