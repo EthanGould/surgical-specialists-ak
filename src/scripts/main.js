45aca8472fda2b2a6b4c294098bc7f61ff2576e5
Bowers96 <bowersc96@gmail.com>
2017-06-03 17:25:11 -0400

    final stretch on ssoa

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -36,6 +36,6 @@ $(document).on('click', 'a', function(event) {
     event.preventDefault();
 
     $('html, body').animate({
-        scrollTop: $($.attr(this, 'href')).offset().top
+        scrollTop: -70 + $($.attr(this, 'href')).offset().top
     }, 500);
 });
